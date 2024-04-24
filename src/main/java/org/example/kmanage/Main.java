1ff1e5feaf979a804d0610bbf13594e1399d6616
shadeisroot <dschristensen@proton.me>
2024-04-24 10:07:15 +0200

    Added login.css and main class

---
+++ src/main/java/org/example/kmanage/Main.java
@@ -0,0 +1,111 @@
+package org.example.kmanage;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+//taken from https://www.javaguides.net/2022/11/javafx-login-form-validation-example.html
+public class Main extends Application {
+
+    private String adminUsername = "admin";
+    private String adminPassword = "admin";
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Login Example");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text sceneTitle = new Text("Login");
+        sceneTitle.setTextAlignment(TextAlignment.CENTER);
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Email Id:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField passwordBox = new PasswordField();
+        grid.add(passwordBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                String username = userTextField.getText().toString();
+                String password = passwordBox.getText().toString();
+
+                if(userTextField.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, "Form Error!",
+                            "Please enter your email id");
+                    return;
+                }
+                if(passwordBox.getText().isEmpty()) {
+                    showAlert(Alert.AlertType.ERROR, "Form Error!",
+                            "Please enter a password");
+                    return;
+                }
+
+                if(adminUsername.equals(username) && adminPassword.equals(password)){
+                    infoBox("Login Successful!", null, "Success");
+                } else{
+                    infoBox("Please enter correct Email and Password", null, "Failed");
+                }
+            }
+        });
+
+        Scene scene = new Scene(grid, 700, 500);
+        scene.getStylesheets().add
+                (Main.class.getResource("Login.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private static void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    public static void infoBox(String infoMessage, String headerText, String title){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setContentText(infoMessage);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.showAndWait();
+    }
+}
