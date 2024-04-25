11daeb278ec330fd14557c7406cac6e586d81723
shadeisroot <dschristensen@proton.me>
2024-04-25 10:50:40 +0200

    Added Database access

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.kmanage {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires com.calendarfx.view;
 
 
     opens org.example.kmanage to javafx.fxml;
---
+++ src/main/java/org/example/kmanage/CalenderDAO.java
@@ -0,0 +1,4 @@
+package org.example.kmanage;
+
+public interface CalenderDAO {
+}
