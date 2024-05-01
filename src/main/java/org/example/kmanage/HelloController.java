7011dcb7bd8a20f26f35f945766f4b2cabae30c8
shadeisroot <dschristensen@proton.me>
2024-05-01 10:46:30 +0200

    cleaned up abit

--- src/main/java/org/example/kmanage/HelloController.java
+++ src/main/java/org/example/kmanage/HelloController.java
@@ -3,13 +3,11 @@ package org.example.kmanage;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class HelloController {
 
