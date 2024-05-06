52c536a27454a3b7a258dd4af175460c96c56733
Jakoc <damernesjacob@gmail.com>
2024-05-06 12:23:47 +0200

    søge felter (virker ikke) lidt mere to projekter

--- src/main/java/org/example/kmanage/User/Project.java
+++ src/main/java/org/example/kmanage/User/Project.java
@@ -1,16 +1,23 @@
 package org.example.kmanage.User;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Project {
     private String name;
     private LocalDate startDate;
     private LocalDate endDate;
+    private String notes;
+    private List<String> files;
+
 
     public Project(String name, LocalDate startDate, LocalDate endDate) {
         this.name = name;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.notes = "";
+        this.files = new ArrayList<>();
     }
 
     public String getName() {
@@ -35,5 +42,25 @@ public class Project {
 
     public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<String> getFiles() {
+        return files;
+    }
+
+    public void addFiles(String files) {
+        this.files.add(files);
+    }
+
+    public void removeFiles(String files) {
+        this.files.remove(files);
     }
 }
