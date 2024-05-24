60e80bc8a2bd3639c904425e6ca4718190bc73c2
shadeisroot <dschristensen@proton.me>
2024-05-24 10:22:27 +0200

    added dropdown in edit profile

--- src/main/java/org/example/kmanage/Classes/Editprofile.java
+++ src/main/java/org/example/kmanage/Classes/Editprofile.java
@@ -5,6 +5,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -34,25 +35,27 @@ public class Editprofile {
         nameField.setText(loggedInUser.getProfile().getName());
         TextField positionField = new TextField();
         positionField.setText(loggedInUser.getProfile().getPosition());
-        TextField departmentField = new TextField();
-        departmentField.setText(loggedInUser.getProfile().getDepartment());
+        ComboBox<String> departmentDropdown = new ComboBox<>();
         Button editbutton = new Button("Ændre profil");
         //tilføjer label og fields til pane
         pane.add(new Label("Navn:"), 0, 0);
         pane.add(nameField, 1, 0);
         pane.add(new Label("Position:"), 0, 1);
         pane.add(positionField, 1, 1);
+        departmentDropdown.getItems().addAll("Tønder Bibliotekerne", "Tønder Kulturskole", "Tønder Medborgerhus", "Schweizerhalle");
+        // Set the default value
+        departmentDropdown.setValue(loggedInUser.getProfile().getDepartment());
         pane.add(new Label("Afdeling"), 0, 2);
-        pane.add(departmentField, 1, 2);
+        pane.add(departmentDropdown, 1, 2);
         pane.add(editbutton, 1, 3);
+
         //action for edit knap (setter name, positio og department ud fra hvad man har skrevet i passende felt
         editbutton.setOnAction(e -> {
             loggedInUser.getProfile().setName(nameField.getText());
             loggedInUser.getProfile().setPosition(positionField.getText());
-            loggedInUser.getProfile().setDepartment(departmentField.getText());
+            loggedInUser.getProfile().setDepartment(departmentDropdown.getValue());
             try {
-                edi.editprofile(nameField.getText(), positionField.getText(), departmentField.getText(), loggedInUser.getProfile().getId());
-                helloController.refreshplist();
+                edi.editprofile(nameField.getText(), positionField.getText(), departmentDropdown.getValue(), loggedInUser.getProfile().getId());
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
